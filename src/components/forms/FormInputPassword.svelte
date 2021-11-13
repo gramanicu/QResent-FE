4af3940b27e540e32204bcbf7c9fde45a4a481e0
WansE1z <44610199+WansE1z@users.noreply.github.com>
2021-11-13 15:46:27 +0200

    working on login

--- src/components/forms/FormInputPassword.svelte
+++ src/components/forms/FormInputPassword.svelte
@@ -1,11 +1,29 @@
 <script>
 	export let label;
 	export let placeholder;
+	let visible = false;
+
+	function toggleViewPass() {
+		visible = !visible;
+	}
 </script>
 
 <div class="form-control">
 	<label class="label">
 		<span class="label-text">{label}</span>
 	</label>
-	<input type="password" {placeholder} class="input" />
+	<div class="relative">
+		<input
+			id="password"
+			type={visible ? 'text' : 'password'}
+			{placeholder}
+			class="input input-primary input-bordered password" />
+		<button type="button" class="btn btn-primary text-neutral-content" on:click={toggleViewPass}>Hide</button>
+	</div>
 </div>
+
+<style>
+	.password {
+		width: 15.62rem;
+	}
+</style>
--- src/routes/auth/signin.svelte
+++ src/routes/auth/signin.svelte
@@ -5,12 +5,31 @@
 </script>
 
 <div class="flex flex-col justify-center items-center w-full h-full">
-	<div class="w-64 h-64">
-		<FormInputText placeholder="Username" label="Username" />
+	<div class="login-reg-container rounded-lg w-96 p-8 shadow-lg">
+		<!-- <FormInputText placeholder="Username" label="Username" /> -->
 		<FormInputEmail placeholder="Email" label="Email" />
 		<FormInputPassword placeholder="Password" label="Password" />
+		<div class="w-full text-right mt-4">
+			<a href="#">Forgot password?</a>
+		</div>
+		<button class="btn btn-primary btn-block mt-6 text-neutral-content">Login</button>
+		<div class="inline-block text-center mt-5 rememberme-div">
+			<p>Remember me?<input type="checkbox" class="checkbox-remember checkbox checkbox-primary checkbox-sm" /></p>
+		</div>
 	</div>
 </div>
 
 <style>
+	.login-reg-container {
+		border: none;
+	}
+
+	.checkbox-remember {
+		margin-left: 10px;
+	}
+
+	.rememberme-div p,
+	.rememberme-div input {
+		vertical-align: middle;
+	}
 </style>
