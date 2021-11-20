1330f89226822590a096017b481ab08a30cc2f21
WansE1z <44610199+WansE1z@users.noreply.github.com>
2021-11-20 14:44:21 +0200

    landing page implemented closes #24

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,8 +7,28 @@
     <meta name="description" content="QResent is a tool used to check online classes attendance" />
 </svelte:head>
 
-<h1>HOMEPAGE</h1>
-<a href="/home">Click ME</a>
+<div class="hero min-h-screen bg-color">
+    <div class="hero-overlay bg-opacity-60" />
+    <div class="text-center hero-content text-neutral-content">
+        <div class="max-w-md">
+            <h1 class="mb-5 text-5xl font-bold">QResent</h1>
+            <p class="mb-5">Nick issue #radusmecher tb completat descriere xoxo</p>
+            <div class="flex flex-row items-center justify-center">
+                <a href="/auth/signin" class="btn btn-primary btn-config text-neutral-content">Login</a>
+                <a href="/auth/signup" class="btn btn-primary btn-config text-neutral-content">Signup</a>
+            </div>
+        </div>
+    </div>
+</div>
 
 <style>
+    .btn-config {
+        margin-left: 10px;
+        margin-right: 10px;
+    }
+
+    .bg-color {
+        background: #ffffff;
+        background: linear-gradient(135deg, hsla(var(--n)) -10%, hsla(var(--p)) 70%, hsla(var(--pf)) 105%);
+    }
 </style>
